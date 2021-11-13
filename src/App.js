ca33a6afeaee83864ff4906e14141e97171181f2
AmitThakur11 <75079562+AmitThakur11@users.noreply.github.com>
2021-11-13 09:43:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import FormInput from "./components/FormInput/";
+export default function App() {
+  return (
+    <div className="App">
+      <form></form>
+    </div>
+  );
+}
